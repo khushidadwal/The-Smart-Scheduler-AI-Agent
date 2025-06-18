37eded914c40237dab247d2bdf706c8f11b0142b
Khushi Dadwal <140509669+khushidadwal@users.noreply.github.com>
2025-06-18 22:38:38 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDEduTdCZA4l05jR7OfrLlmZrapx0orXC8")  # <-- replace with your real key
-
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-response = model.generate_content("What's the capital of France?")
-print(response.text)
